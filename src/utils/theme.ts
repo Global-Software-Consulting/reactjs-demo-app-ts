2c69e997aa7305a9bff20ee2f9859ccfc24f18a0
waseem mansha <waseem.mansha@gmail.com>
2021-10-12 14:37:23 +0500

    basic setup

---
+++ src/utils/theme.ts
@@ -0,0 +1,43 @@
+import { PaletteMode } from '@mui/material';
+import { createTheme } from '@mui/material/styles'
+
+let allThemes : Array<PaletteMode>=['light', 'dark']
+export const themes = allThemes.map((mode: PaletteMode) => {
+  console.log('mode', mode)
+  return createTheme(
+    {
+      palette: {
+        mode,
+        primary: {
+          main: mode === 'light' ? 'rgb(24,119,242)' : 'rgb(45,136,255)'
+        },
+        background: {
+          default: mode === 'light' ? '#fff' : 'rgb(24,25,26)'
+        },
+       
+      },
+
+      typography: {
+        fontFamily: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI'
+        ].join(',')
+      }
+    },
+    {
+      typography: {
+        h1: { fontSize: '2em' },
+        h2: { fontSize: '1.5em' },
+        h3: { fontSize: '1.3em' },
+        h5: { fontSize: '0.8em' },
+        h6: { fontSize: '0.7em' },
+        button: {
+          textTransform: 'none',
+          background: mode === 'light' ? '#fff' : '#rgb(45,136,255)',
+          color: mode === 'light' ? '#000' : '#fff'
+        }
+      }
+    }
+  )
+})
